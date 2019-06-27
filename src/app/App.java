30afae8c609ed7271dec1c37e109aec07bd0c0de
Pineapple42 <frozenfire71@gmail.com>
2019-06-27 11:11:05 -0500

    Testing
    Still trying to get the hang of constructors, classes and stuff. It works so far, though.

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,25 @@
 package app;
 
+import java.util.Scanner;
+import app.TaggingMode;
+
 public class App {
+
+    public static Scanner kb = new Scanner(System.in);
+    
+    private App() {
+
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        String choice = "";
+        String versionNumber = "v0.0.0";
+
+        System.out.println("Welcome to the Image Tagging app " + versionNumber + ".");
+        System.out.println("What would you like to do?");
+        System.out.println("T = Tagging mode");
+        System.out.println("V = Viewing mode");
+        choice = kb.next();
+        System.out.println("Your choice was: " + choice);
+        TaggingMode.choose();
     }
 }
