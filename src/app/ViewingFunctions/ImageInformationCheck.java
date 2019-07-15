2f0517f68b35f0f2fe852c267bf39ed28fbcc6d5
Pineapple42 <frozenfire71@gmail.com>
2019-07-14 21:32:09 -0500

    Added functions for ViewingMode
    Still a bunch of placeholder text stuff lol

---
+++ src/app/ViewingFunctions/ImageInformationCheck.java
@@ -0,0 +1,11 @@
+package app.ViewingFunctions;
+
+public class ImageInformationCheck{
+    private ImageInformationCheck(){
+
+    }
+
+    public static void main(){
+        System.out.println("Checking Image stuff...");
+    }
+}
---
+++ src/app/ViewingFunctions/TagInformationCheck.java
@@ -0,0 +1,11 @@
+package app.ViewingFunctions;
+
+public class TagInformationCheck{
+    private TagInformationCheck(){
+
+    }
+
+    public static void main(){
+        System.out.println("Checking pictures assigned to tag...");
+    }
+}
