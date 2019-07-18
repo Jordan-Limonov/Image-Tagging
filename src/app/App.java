f8be928a2618f1e15a2c4203bdff43e6b85182ea
Pineapple42 <frozenfire71@gmail.com>
2019-07-17 20:58:12 -0500

    started integrating java swing
    still in testing phases

--- src/app/App.java
+++ src/app/App.java
@@ -1,34 +1,37 @@
 package app;
 
 import java.util.Scanner;
-import app.TaggingMode;
-import app.ViewingMode;
+
+import javax.swing.*;
 
 public class App {
 
     public static Scanner kb = new Scanner(System.in);
     public static String[] placeholder = new String[20];
 
+    static String versionNumber = "v0.0.0";
+
+    static JFrame frame = new JFrame();
+    static JLabel welcomeMessage = new JLabel("Welcome to the Image Tagging app " + versionNumber + ".");
+
     private App() {
 
     }
-    public static void main(String[] args) throws Exception {
-        String choice = "";
-        String versionNumber = "v0.0.0";
 
-        System.out.println("Welcome to the Image Tagging app " + versionNumber + ".");
-        System.out.println("What would you like to do?");
-        System.out.println("T = Tagging mode");
-        System.out.println("V = Viewing mode");
-        choice = kb.next();
-        // if(choice == "V" || choice == "v"){
-        //     ViewingMode.choose();
-        // }else if(choice == "T" || choice == "t"){
-        //     TaggingMode.choose();
-        // }else{
-        //     System.out.println("Please choose V or T");
-        // }
-        selectMode(choice);
+    public static void main(String[] args) throws Exception {
+        welcomeMessage.setHorizontalAlignment();
+        
+        frame.add(welcomeMessage);
+        frame.setSize(640, 480);
+        frame.setVisible(true);
+        // String choice = "";
+
+        // System.out.println("What would you like to do?");
+        // System.out.println("T = Tagging mode");
+        // System.out.println("V = Viewing mode");
+        // choice = kb.next();
+
+        // selectMode(choice);
     }
     public static void selectMode(String mode) throws Exception {
         if(mode.equals("V") || mode.equals("v")){
--- src/app/ViewingFunctions/ImageInformationCheck.java
+++ src/app/ViewingFunctions/ImageInformationCheck.java
@@ -1,11 +1,28 @@
 package app.ViewingFunctions;
 
-public class ImageInformationCheck{
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
+public class ImageInformationCheck {
+
+    public static Scanner kb = new Scanner(System.in);
+    static String imagePath = "";
+    
     private ImageInformationCheck(){
 
     }
 
-    public static void main(){
+    public static void main() {
         System.out.println("Checking Image stuff...");
+        System.out.println("");
+        imagePath = kb.next();
+        loadImage();
+    }
+
+    public static void loadImage() {
+
     }
 }
